bab95552b9f4d940b8f263059ee02b934cd1c66c
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-13 13:48:17 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,7 +41,7 @@
 #     Species
 #   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
 #   # HoH all the way to Species!
-{
+ {
     label: "Kingdom",
     sub_category: {
       label: "Phylum",
@@ -55,7 +55,5 @@
               label: "Genus",
               sub_category: {
                 label: "Species",
-                sub_category: nil } } } } } } 
-  
-}
+                sub_category: nil } } } } } } }
 end
